bde5117d2fff13537ffee2f3b8545b5851ba3c6b
J-herika <138695574+J-Herika@users.noreply.github.com>
2024-05-22 18:19:09 +0300

    add func to get and add new book

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+
 const myLibrary = [];
+
+const addBook = document.querySelector('.add-book')
 
 function Book(title, author, numOfPages, readStatus){
   //the constructor
@@ -8,11 +11,35 @@ function Book(title, author, numOfPages, readStatus){
   this.readStatus = readStatus;
 }
 
-function addBookToLibrary(){
-  //do stuff here
+function getBookData(){
+  let book;
+
+  title = prompt('Enter the book title', 'Harry Potter')
+  author = prompt('Enter the book author', 'Harry Rich')
+  numOfPages = prompt('Enter the number of pages in the book', '213')
+  readStatus = prompt('Did you read the book?','Read,Not Read, Still Reading')
+
+  book = new Book(title,author,numOfPages,readStatus);
+  addBookToLibrary(book)
 }
 
 
+function addBookToLibrary(bookToAdd){
+  myLibrary.push(bookToAdd);
+ 
+  //just to debugging
+  console.log(myLibrary)
+}
+
+//button for adding a new book
+addBook.addEventListener('click',getBookData)
+
+
+
+
+
+// i want to get user input and store the data in a new Book
+// then i want to add that book to the library array
 
 
 
