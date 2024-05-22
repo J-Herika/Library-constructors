b39b01db49c78747a90b3b511a540cf405ba9037
J-herika <138695574+J-Herika@users.noreply.github.com>
2024-05-22 17:21:39 +0300

    did the book constructor

---
+++ script.js
@@ -0,0 +1,21 @@
+const myLibrary = [];
+
+function Book(title, author, numOfPages, readStatus){
+  //the constructor
+  this.title = title;
+  this.author = author;
+  this.numOfPages = numOfPages;
+  this.readStatus = readStatus;
+}
+
+function addBookToLibrary(){
+  //do stuff here
+}
+
+
+
+
+
+
+
+// const book1 = new Book('dont give up', 'you', 1, 'read')
