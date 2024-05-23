17941908727e00417f835503f0ca5f030026e0bd
J-herika <138695574+J-Herika@users.noreply.github.com>
2024-05-23 17:18:16 +0300

    finished displaying the book

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-
 const myLibrary = [];
 
-const addBook = document.querySelector('.add-book')
-const displayContent = document.querySelector('.display-content')
-const bookContent = document.querySelector('.book-content')
-const bookDetails = document.querySelector('.book-details')
+const addBook = document.querySelector(".add-book");
+const displayContent = document.querySelector(".display-content");
+const bookContent = document.querySelector(".book-content");
+const bookDetails = document.querySelector(".book-details");
 
-let title = bookDetails.querySelector('.title');
-let author = bookDetails.querySelector('.author');
-let pages = bookDetails.querySelector('.pages');
-let status = bookDetails.querySelector('.status');
+let title = bookDetails.querySelector(".title");
+let author = bookDetails.querySelector(".author");
+let pages = bookDetails.querySelector(".pages");
+let status = bookDetails.querySelector(".status");
 
-function Book(title, author, numOfPages, readStatus){
+function Book(title, author, numOfPages, readStatus) {
   //the constructor
   this.title = title;
   this.author = author;
@@ -19,29 +18,41 @@ function Book(title, author, numOfPages, readStatus){
   this.readStatus = readStatus;
 }
 
-function getBookData(){
+function getBookData() {
   let book;
 
-  title = prompt('Enter the book title', 'Harry Potter')
-  author = prompt('Enter the book author', 'Harry Rich')
-  numOfPages = prompt('Enter the number of pages in the book', '213')
-  readStatus = prompt('Did you read the book?','Read,Not Read, Still Reading')
+  title = prompt("Enter the book title", "Harry Potter");
+  author = prompt("Enter the book author", "Harry Rich");
+  numOfPages = prompt("Enter the number of pages in the book", "213");
+  readStatus = prompt("Did you read the book?", "Read,Not Read, Still Reading");
 
-  book = new Book(title,author,numOfPages,readStatus);
-  addBookToLibrary(book)
+  book = new Book(title, author, numOfPages, readStatus);
+  addBookToLibrary(book);
+  displayBooks();
 }
 
-function addBookToLibrary(bookToAdd){
+function addBookToLibrary(bookToAdd) {
   myLibrary.push(bookToAdd);
- 
+
   //just to debugging
-  console.log(myLibrary)
+  console.log(myLibrary);
 }
 
-function displayBooks(){
-  myLibrary.forEach(book => {
-    console.log(book);
-  });
+function displayBooks() {
+  let book = myLibrary.at(-1)
+  let bookContainer = bookContent.cloneNode(true);
+
+  let title = bookContainer.querySelector(".title");
+  let author = bookContainer.querySelector(".author");
+  let pages = bookContainer.querySelector(".pages");
+  let status = bookContainer.querySelector(".status");
+
+  title.textContent = book.title;
+  author.textContent = `Author: ${book.author}`;
+  pages.textContent = `Pages: ${book.numOfPages}`;
+  status.textContent = `Status: ${book.readStatus}`;
+
+  displayContent.appendChild(bookContainer);
 }
 //how to display the array of books
 //step 1 get access to "book-content" DONE
@@ -51,21 +62,14 @@ function displayBooks(){
 //step 4 and create a title,author,pages and status div.
 // step 5 now you can add the correct content to the div's you created up.
 
-
-
 //button for adding a new book
-addBook.addEventListener('click',getBookData)
+addBook.addEventListener("click", getBookData);
 let clone = bookContent.cloneNode(true);
 displayContent.appendChild(clone);
 
-displayBooks();
 
 
 // i want to get user input and store the data in a new Book
 // then i want to add that book to the library array
-
-
-
-
 
 // const book1 = new Book('dont give up', 'you', 1, 'read')
