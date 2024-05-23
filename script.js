211b2feabf9968e70e4a18dfd20d93122d143520
J-herika <138695574+J-Herika@users.noreply.github.com>
2024-05-23 12:30:25 +0300

    did the first 2 steps in displaying an array of books

--- script.js
+++ script.js
@@ -2,6 +2,14 @@
 const myLibrary = [];
 
 const addBook = document.querySelector('.add-book')
+const displayContent = document.querySelector('.display-content')
+const bookContent = document.querySelector('.book-content')
+const bookDetails = document.querySelector('.book-details')
+
+let title = bookDetails.querySelector('.title');
+let author = bookDetails.querySelector('.author');
+let pages = bookDetails.querySelector('.pages');
+let status = bookDetails.querySelector('.status');
 
 function Book(title, author, numOfPages, readStatus){
   //the constructor
@@ -23,14 +31,6 @@ function getBookData(){
   addBookToLibrary(book)
 }
 
-//how to display the array of books
-//step 1 get access to "book-details"
-//step 2 get access to the children elements like title,author,pages,status.
-//step 3 make a function that loops for every object book in the myLibrary
-// it will a div in "book-content" add to it the class "book-details".
-//step 4 and create a title,author,pages and status div.
-// step 5 now you can add the correct content to the div's you created up.
-
 function addBookToLibrary(bookToAdd){
   myLibrary.push(bookToAdd);
  
@@ -38,11 +38,27 @@ function addBookToLibrary(bookToAdd){
   console.log(myLibrary)
 }
 
+function displayBooks(){
+  myLibrary.forEach(book => {
+    console.log(book);
+  });
+}
+//how to display the array of books
+//step 1 get access to "book-content" DONE
+//step 2 get access to the children elements like title,author,pages,status.
+//step 3 make a function that loops for every object book in the myLibrary
+// it will a div in "book-content" add to it the class "book-details".
+//step 4 and create a title,author,pages and status div.
+// step 5 now you can add the correct content to the div's you created up.
+
+
+
 //button for adding a new book
 addBook.addEventListener('click',getBookData)
+let clone = bookContent.cloneNode(true);
+displayContent.appendChild(clone);
 
-
-
+displayBooks();
 
 
 // i want to get user input and store the data in a new Book
