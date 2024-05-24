221eb0efb533833c2108b13fb02e19b9933fd712
J-herika <138695574+J-Herika@users.noreply.github.com>
2024-05-24 11:09:36 +0300

    made the form work

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ let author = bookDetails.querySelector(".author");
 let pages = bookDetails.querySelector(".pages");
 let status = bookDetails.querySelector(".status");
 
+let bookForm = document.querySelector('.book-form');
+let formTitle = bookForm[0];
+let formAuthor = bookForm[1];
+let formPages = bookForm[2];
+let formStatus = bookForm[3];
+let formButton = bookForm[4];
+
+
 function Book(title, author, numOfPages, readStatus) {
   //the constructor
   this.title = title;
@@ -21,14 +29,19 @@ function Book(title, author, numOfPages, readStatus) {
 function getBookData() {
   let book;
 
-  title = prompt("Enter the book title", "Harry Potter");
-  author = prompt("Enter the book author", "Harry Rich");
-  numOfPages = prompt("Enter the number of pages in the book", "213");
-  readStatus = prompt("Did you read the book?", "Read,Not Read, Still Reading");
+  title = formTitle.value
+  author = formAuthor.value
+  numOfPages = formPages.value
+  readStatus = formStatus.value
 
   book = new Book(title, author, numOfPages, readStatus);
   addBookToLibrary(book);
   displayBooks();
+
+  formTitle.value = '';
+  formAuthor.value = '';
+  formPages.value = '';
+  formStatus.value = '';
 }
 
 function addBookToLibrary(bookToAdd) {
@@ -57,12 +70,8 @@ function displayBooks() {
 
 
 //button for adding a new book
-addBook.addEventListener("click", getBookData);
+formButton.addEventListener('click', getBookData)
 
 
 
 
-// i want to get user input and store the data in a new Book
-// then i want to add that book to the library array
-
-// const book1 = new Book('dont give up', 'you', 1, 'read')
